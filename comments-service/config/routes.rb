6e817b1245ee9bf54242c01a2279bc014d2f228a
bal7a <singularity717@gmail.com>
2026-04-03 04:56:38 +0200

    Add cors for comments service.

--- comments-service/config/routes.rb
+++ comments-service/config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
 
-  resources :comments, only: [:create]
+  resources :comments, only: %i[create destroy show update]
   get '/tasks/:task_id/comments', to: 'comments#index'
 
   # Defines the root path route ("/")
